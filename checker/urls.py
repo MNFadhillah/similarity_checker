a471a96d078e75872899327762aa42345f539856
MNFadhillah <131852870+MNFadhillah@users.noreply.github.com>
2025-11-01 05:58:39 +0800

    Rename download_result route to 'download'

--- checker/urls.py
+++ checker/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_view, name='upload'),
-    path('download/<str:filename>/', views.download_result_view, name='download_result'),
+    path('download/<str:filename>/', views.download_result_view, name='download'),
 ]
